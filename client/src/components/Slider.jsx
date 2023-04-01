133a3bba0b655be00bf4720c4db4b79343e17dd0
NellyNel520 <channelharris520@gmail.com>
2023-04-01 01:49:26 -0400

    created styled component

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -1,57 +1,111 @@
 import React from 'react'
-import styled from "styled-components";
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import styled from 'styled-components'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos'
 
+const Container = styled.div`
+	width: 100%;
+	height: 100vh;
+	display: flex;
+	position: relative;
+	overflow: hidden;
+`
+
+const Arrow = styled.div`
+	width: 50px;
+	height: 50px;
+	background-color: #fff7f7;
+	border-radius: 50%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	position: absolute;
+	top: 0;
+	bottom: 0;
+	left: ${(props) => props.direction === 'left' && '10px'};
+	right: ${(props) => props.direction === 'right' && '10px'};
+	margin: auto;
+	cursor: pointer;
+	opacity: 0.5;
+	z-index: 2;
+`
 const Wrapper = styled.div`
-  height: 100%;
-  display: flex;
-  transition: all 1.5s ease;
-  transform: translateX(${(props) => props.slideIndex * -100}vw);
-`;
+	height: 100%;
+	display: flex;
+	transition: all 1.5s ease;
+	transform: translateX(${(props) => props.slideIndex * -100}vw);
+`
 const Slide = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  align-items: center;
-  background-color: #${(props) => props.bg};
-`;
+	width: 100vw;
+	height: 100vh;
+	display: flex;
+	align-items: center;
+	background-color: #${(props) => props.bg};
+`
 
 const ImgContainer = styled.div`
-  height: 100%;
-  flex: 1;
-`;
+	height: 100%;
+	flex: 1;
+`
 
 const Image = styled.img`
-  height: 80%;
-`;
+	height: 90%;
+`
 
 const InfoContainer = styled.div`
-  flex: 1;
-  padding: 50px;
-`;
+	flex: 1;
+	padding: 50px;
+`
+
+const Title = styled.h1`
+	font-size: 70px;
+`
+
+const Desc = styled.p`
+	margin: 50px 0px;
+	font-size: 20px;
+	font-weight: 500;
+	letter-spacing: 3px;
+`
+
+const Button = styled.button`
+	padding: 10px;
+	font-size: 20px;
+	background-color: ;
+	cursor: pointer;
+`
 
 const Slider = () => {
-  return (
-    <div className='w-[100%] h-[100vh] flex] relative overflow-hidden'>
-      <div direction="left" className='flex w-[50px] h-[50px] absolute top-0 bottom-0 m-auto left-5 justify-center align-middle'>
-        <ArrowBackIosIcon />
-      </div>
-      <Wrapper>
-        <Slide>
-          <ImgContainer>
-            <Image src="https://i.postimg.cc/13fxzcXc/hoodie-mockup-of-a-man-with-cool-locs-m10777-2.png" />
-          </ImgContainer>
-          <InfoContainer></InfoContainer>
-        </Slide>
-      </Wrapper>
+	return (
+		<Container>
+			<Arrow>
+				<ArrowBackIosIcon />
+			</Arrow>
 
-      <div direction="right" className='flex w-[50px] h-[50px]  absolute top-0 bottom-0 m-auto right-5  justify-center align-middle'>
-        <ArrowForwardIosIcon />
-      </div>
+			<Wrapper>
+				<Slide>
+					<ImgContainer>
+						<Image src="https://i.postimg.cc/13fxzcXc/hoodie-mockup-of-a-man-with-cool-locs-m10777-2.png" />
+					</ImgContainer>
+					<InfoContainer>
+						<Title>SPRING FLING </Title>
+						<Desc>
+							Spring into style with our exclusive Spring sale! Shop now and get
+							amazing discounts on our latest collection of clothing.
+						</Desc>
+						<Button className="bg-blue-400 hover:bg-yellow-300 hover:text-blue-600">
+							SHOP NOW
+						</Button>
+					</InfoContainer>
+				</Slide>
+			</Wrapper>
 
-    </div>
-  )
+			<Arrow>
+				<ArrowForwardIosIcon />
+			</Arrow>
+
+		</Container>
+	)
 }
 
 export default Slider
