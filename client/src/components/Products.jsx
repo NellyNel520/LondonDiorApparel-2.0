aba7157055af0bd7a93ce13e3ffa0ee0af3a854c
NellyNel520 <channelharris520@gmail.com>
2023-04-01 14:37:40 -0400

    created products and product card components and styled using styled components

---
+++ client/src/components/Products.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styled from "styled-components";
+import { popularProducts } from "../data";
+import ProductCard from './ProductCard';
+
+
+
+const Container = styled.div`
+ padding: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
+const Products = () => {
+  return (
+    <Container>
+      {popularProducts.map(item=> (
+        <ProductCard item={item} key={item.id} />
+      ))}
+    </Container>
+  )
+}
+
+export default Products
