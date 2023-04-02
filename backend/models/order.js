f3090c70803671408868440f398c4f03e93935df
NellyNel520 <channelharris520@gmail.com>
2023-04-02 00:47:48 -0400

    exported all models through index js file for better file organization

--- backend/models/order.js
+++ backend/models/order.js
@@ -2,12 +2,10 @@ const mongoose = require("mongoose");
 
 const Order = new Schema(
   {
-    userId: { type: String, required: true },
+    userId:  {type: Schema.Types.ObjectId, ref: 'User', required:true},
     products: [
       {
-        productId: {
-          type: String,
-        },
+        productId: {type: Schema.Types.ObjectId, ref: 'Product', required:true},
         quantity: {
           type: Number,
           default: 1,
