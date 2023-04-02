3c800476fc0d46f5411bfc4913db2cf8d9ab91b4
NellyNel520 <channelharris520@gmail.com>
2023-04-02 01:13:39 -0400

    completed auth router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,9 @@
 const Router = require("express").Router();
+const AuthRouter = require("./AuthRouter")
 
 // Test
 Router.get('/', (req, res) => res.send('This is root!'))
+
+Router.use("/auth", AuthRouter)
 
 module.exports = Router;
--- client/src/App.js
+++ client/src/App.js
@@ -5,10 +5,12 @@ import ProductCategory from "./pages/ProductCategory";
 import ProductDetails from "./pages/ProductDetails";
 import Register from "./pages/Register";
 import './styles/App.css'
+import { useState } from 'react'
 
 
 
 function App() {
+  const [user, setUser] = useState(null)
   return (
     <div>
      <Home />
