4e18468650c9564375c072e15b52694a414c3137
NellyNel520 <channelharris520@gmail.com>
2023-04-02 14:58:40 -0400

    completed category controllers

--- backend/controllers/CategoryController.js
+++ backend/controllers/CategoryController.js
@@ -38,9 +38,36 @@ const getCategoryById = async (req, res) => {
 	}
 };
 
+// update category
+const updateCategory = async (req, res) => {
+  try {
+      const category = await  Category.findByIdAndUpdate(req.params.id, req.body, { new: true})
+      res.status(200).json(category)
+  } catch (error) {
+      return res.status(500).send(error.message);
+  }
+}
+
+// delete category
+const deleteCategory = async (req, res) => {
+  try {
+      const { id } = req.params;
+      const deleted = await Category.findByIdAndDelete(id)
+      if (deleted) {
+          return res.status(200).send("Category deleted");
+      }
+      throw new Error("Category not found");
+  } catch (error) {
+      return res.status(500).send(error.message);
+  }
+}
+
+
 module.exports ={
   createProductCategory,
   getAllCategories,
   getCategoryById,
+  updateCategory,
+  deleteCategory
 
 }
