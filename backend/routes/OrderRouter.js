ef63af3f428445674207f6743217e089f2bad2a2
NellyNel520 <channelharris520@gmail.com>
2023-04-03 01:03:17 -0400

    added protected routes to order router

--- backend/routes/OrderRouter.js
+++ backend/routes/OrderRouter.js
@@ -1,20 +1,37 @@
 const router = require("express").Router();
 const controllers = require("../controllers/OrderController");
 
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require('../middleware')
+// ROUTE PROTECTION NOTES
+// verifyTokenAndAuthorization:  checks jwt token and if auth 
+// verifyTokenAndAdmin: checks jwt token and isAdmin status (admin dashboard icebox feature)
+  // verifyToken: checks jwt token 
+
+
 // orders will need protected routes admin only once auth is completed
 
-// create order
-router.post('/newOrder', controllers.createOrder)
+// create order (must create a account & login to place an order)
+router.post('/newOrder', verifyToken,  controllers.createOrder)
 
-// Get All orders (Read)
-router.get('/orders', controllers.getAllOrders)
+// Get All orders (Read) || admin function
+router.get('/orders', verifyTokenAndAdmin,  controllers.getAllOrders)
+
 // Get by order ID
 router.get('/orders/:userId', controllers.getOrderById)
-// Get User order (by user id)
-router.get('/order/:id', controllers.getOrderById)
-// UPDATE
-router.put('/updateOrder/:id', controllers.updateOrder)
-// DELETE
-router.delete('/deleteOrder/:id', controllers.deleteOrder)
+
+// Get User order (by user id) login required
+router.get('/order/:id', verifyTokenAndAuthorization,  controllers.getOrderById)
+
+// Get monthly income || admin only function (admin dashboard icebox)
+router.get('/order/:id', verifyTokenAndAdmin,  controllers.getMonthlyIncome)
+
+// UPDATE || admin only functionality
+router.put('/updateOrder/:id', verifyTokenAndAdmin,  controllers.updateOrder)
+// DELETE || admin only functionality
+router.delete('/deleteOrder/:id', verifyTokenAndAdmin, controllers.deleteOrder)
 
 module.exports = router
