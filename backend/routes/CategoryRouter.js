bee9c727107a73b4b73eb9a52f6dfa45514fb585
NellyNel520 <channelharris520@gmail.com>
2023-04-03 01:14:21 -0400

    route protection for admin functions category rputer

--- backend/routes/CategoryRouter.js
+++ backend/routes/CategoryRouter.js
@@ -1,17 +1,27 @@
 const router = require("express").Router();
 const controllers = require("../controllers/CategoryController");
 
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require('../middleware')
+// ROUTE PROTECTION NOTES
+// verifyTokenAndAuthorization:  checks jwt token and if auth 
+// verifyTokenAndAdmin: checks jwt token and isAdmin status (admin dashboard icebox feature)
+  // verifyToken: checks if logged in (jwt token generated after every successful login) 
+
 
 // create category
-router.post('/addCategory', controllers.createCategory)
+router.post('/addCategory', verifyTokenAndAdmin, controllers.createCategory)
 
 // Get All Categories (Read)
 router.get('/category', controllers.getAllCategories)
 // Get by product ID
 router.get('/category/:id', controllers.getCategoryById)
 // UPDATE
-router.put('/updateCategory/:id', controllers.updateCategory)
+router.put('/updateCategory/:id', verifyTokenAndAdmin, controllers.updateCategory)
 // DELETE
-router.delete('/deleteCategory/:id', controllers.deleteCategory)
+router.delete('/deleteCategory/:id', verifyTokenAndAdmin, controllers.deleteCategory)
 
 module.exports = router
