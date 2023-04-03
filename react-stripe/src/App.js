fa66da2099eb475cc5f21f4a77559853f85c671d
NellyNel520 <channelharris520@gmail.com>
2023-04-03 13:30:38 -0400

    created test app for stripe api checkout couldnt use insomnia or postman to test connection

--- react-stripe/src/App.js
+++ react-stripe/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import './index.css';
+import { Route, Routes } from 'react-router-dom'
+import Pay from './pages/Pay';
+import Success from './pages/Success';
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/pay" element={<Pay />} />
+        <Route path="/success" element={<Success />}/>
+      </Routes>
       
     </div>
   );
