b2a0c2b3b629cf677a2f5346a85b99656e3cfa3f
NellyNel520 <channelharris520@gmail.com>
2023-04-03 00:54:04 -0400

    added route protection to product router to protect admin only functions

--- backend/routes/ProductRouter.js
+++ backend/routes/ProductRouter.js
@@ -1,16 +1,25 @@
 const router = require('express').Router()
 const controllers = require('../controllers/ProductController')
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require('../middleware')
+// ROUTE PROTECTION NOTES
+// verifyTokenAndAuthorization:  checks jwt token and if auth 
+// verifyTokenAndAdmin: checks jwt token and isAdmin status (admin dashboard icebox feature)
+  // verifyToken: checks jwt token 
 
-// create product
-router.post('/addProduct', controllers.createProduct)
+// create product || protected admin only
+router.post('/addProduct',verifyTokenAndAdmin,  controllers.createProduct)
 
 // Get All products (Read)
 router.get('/products', controllers.getAllProducts)
 // Get by product ID
 router.get('/products/:id', controllers.getProductById)
-// UPDATE
-router.put('/updateProduct/:id', controllers.updateProduct)
-// DELETE
-router.delete('/deleteProduct/:id', controllers.deleteProduct)
+// UPDATE || protected admin only
+router.put('/updateProduct/:id', verifyTokenAndAdmin, controllers.updateProduct)
+// DELETE || protected admin onl
+router.delete('/deleteProduct/:id',verifyTokenAndAdmin, controllers.deleteProduct)
 
 module.exports = router
